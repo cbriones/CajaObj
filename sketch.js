1d2ea51ebe2fc7c1771428ad826604547edef2f6
Claudio Briones <briones.jerez@gmail.com>
2017-06-22 23:39:35 -0400

    cleaning a bit

--- sketch.js
+++ sketch.js
@@ -1,24 +1,3 @@
-// var i = 0;
-// var d = 1;
-//var micaja = cajita;
-// var foo1 = new foo();
-// var foo2 = foo;
-// var foo3 = foo;
-
-// var bar2 = barObj;
-
-// var micaja1;
-// function foo (){
-//   foox = 88;
-// };
-
-// var barObj = {
-//   x: 69,
-//   y: 70
-// }
-
-// var bar1 = Object.create(barObj);
-
 var cajas = [];
 var n_vertical = 500;
 
@@ -29,21 +8,7 @@ function setup() {
    for(var i = 0; i < n_vertical; i++){
     cajas[i] = new CajaObj(i*windowHeight/n_vertical);
    }
-
 }
-
-// function cajita(){
-//   this.x = 100;
-//   this.y = 100;
-
-//   mover = function() {
-//     x++;
-//   }
-//   this.mostrar = function(){
-//     rect(this.x, this.y, 20, 20);
-//   }
-
-// }
 
 function CajaObj(y){
   var d = 1;
@@ -53,9 +18,6 @@ function CajaObj(y){
   this.y = y;
 
   this.mover = function() {
-    //var d = -1;
-    //console.log(this.x);
-    //var i = 0; 
     if (this.x > windowWidth-100)
       {d = -1;}
     else if (this.x < 0) {d = 1;}
@@ -79,15 +41,4 @@ function draw() {
     cajas[i].mover();
     cajas[i].mostrar();
   }
-  //console.log(micaja1.x);
-  //rect(200,200, 30, 30);
-  
-  // for(var k= 10; k < 400; k = k + 40){
-  // rect(i - k, k, 20, 20);}
-  // if (i == 400-20){d = -1;}
-  //     else if(i === 0) {d = 1;}
-  
-  // i = i + d;
-  
-
 }
